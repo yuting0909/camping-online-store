fab86c840edc3be2fc45497a941654bd3f6b5357
Browny <kyul4852@gmail.com>
2021-09-30 11:21:26 +0800

    Create ProductSection2.vue as component

---
+++ src/components/ProductSection2.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="product-section-2">
+    <div class="container py-5">
+      <div class="row">
+        <div class="col-md-3">
+          <h2 class="fs-4 mb-3">
+            <i class="bi bi-tree-fill text-success me-2"></i>選擇營地
+          </h2>
+        </div>
+        <div class="col-md-9">
+          <ul class="list-unstyled">
+            <li
+              v-for="(type, i) in types"
+              :key="i"
+              class="card rounded-3 overflow-hidden mb-3 border-0 bg-light"
+            >
+              <div class="row g-0">
+                <div class="col-5">
+                  <img class="type-pic" :src="type.images[0]" alt="營地圖片" />
+                </div>
+                <div class="col-7"></div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.type-pic {
+  object-fit: cover;
+  width: 100%;
+  height: 200px;
+}
+</style>
+
+<script>
+export default {
+  props: ['types']
+}
+</script>
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -16,49 +16,23 @@
   <Header></Header>
   <main :id="`product-introduction${id}`" class="product-intro pt-5 pt-md-0">
     <product-section-1 :product="product" :features="features"></product-section-1>
-    <div class="product-section-2">
-      <div class="container py-5">
-        <div class="row">
-          <div class="col-md-3">
-            <h2 class="fs-4 mb-3"><i class="bi bi-tree-fill text-success me-2"></i>選擇營地</h2>
-          </div>
-          <div class="col-md-9">
-            <ul class="list-unstyled">
-              <li v-for="(type, i) in types" :key="i" class="card rounded-3 overflow-hidden mb-3 border-0 bg-light">
-                <div class="row g-0">
-                  <div class="col-5">
-                    <img class="type-pic" :src="type.images[0]" alt="營地圖片">
-                  </div>
-                  <div class="col-7"></div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
+    <product-section-2 :types="types"></product-section-2>
   </main>
   <Footer></Footer>
 </template>
-
-<style lang="scss">
-.type-pic {
-  object-fit: cover;
-  width: 100%;
-  height: 200px;
-}
-</style>
 
 <script>
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
 import ProductSection1 from '@/components/ProductSection1.vue'
+import ProductSection2 from '@/components/ProductSection2.vue'
 
 export default {
   components: {
     Header,
     Footer,
-    ProductSection1
+    ProductSection1,
+    ProductSection2
   },
   data () {
     return {
